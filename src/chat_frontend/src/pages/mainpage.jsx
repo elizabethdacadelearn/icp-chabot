00d7eac725c040488d9b0909337567d80724057d
elizabethdacadelearn <elizabethvin684@gmail.com>
2025-01-26 23:32:05 +0300

    first commit

--- src/chat_frontend/src/pages/mainpage.jsx
+++ src/chat_frontend/src/pages/mainpage.jsx
@@ -1,22 +1,40 @@
 import { useEffect, useState } from "react";
 import { chat_backend } from "declarations/chat_backend";
 import { useNavigate } from "react-router-dom";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 const MainPage = () => {
   const [search, setSearch] = useState("");
 
   const navigate = useNavigate();
   const [data, setData] = useState([]);
-  const [res, setRes] = useState();
+  const [res, setRes] = useState("");
   useEffect(() => {
     chat_backend.UserHistory().then((result) => {
+      console.log(result);
       setData(result[0].history);
     });
   }, []);
 
-  const handlesubmit = (e) => {
+  const genAi = new GoogleGenerativeAI(
+   
+  );
+
+  const handlesubmit = async (e) => {
     e.preventDefault();
+    const model = genAi.getGenerativeModel({ model: "gemini-pro" });
+
+    const prompt = `answer user question based on user search that is ${search} and should be less than 300 words`;
+
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+
+    setRes(text);
     chat_backend.update(search, res).then((result) => {
       console.log(result, "seearch user");
+    });
+    chat_backend.UserHistory().then((result) => {
+      setData(result[0].history);
     });
   };
   const handleclerahistory = () => {
@@ -33,16 +51,16 @@ const MainPage = () => {
         <div className="rt">
           <h1 className="">icpGPT</h1>
           <div className="mt-3">
-            {!data ? (
-              <div className="text-center">
-                <h1 className="font-bold">No search history</h1>
+            {!data || data.length == 0 ? (
+              <div className="">
+                <h1 className="yu">No search history</h1>
               </div>
             ) : (
               <div>
                 {data.map((val, _index) => (
                   <div className="" key={_index}>
                     <p
-                      className="font-bold text-sm truncate cursor-pointer"
+                      className="y"
                       onClick={() => setRes(val.response)}
                     >
                       {val.request}
