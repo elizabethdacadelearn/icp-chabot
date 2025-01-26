1386185da19d61ab20f32736f38933672c7b2293
elizabethdacadelearn <elizabethvin684@gmail.com>
2025-01-26 23:00:46 +0300

    first commit

--- src/chat_frontend/src/App.jsx
+++ src/chat_frontend/src/App.jsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/welcome";
 import MainPage from "./pages/mainpage";
+import { AuthProvider } from "./auth/authetication";
 function App() {
   
   return (
     <BrowserRouter>
+    <AuthProvider>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/find" element={<MainPage/>} />
       </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
